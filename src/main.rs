96f9802de84aca710bbdb44fdee23f7b2f2703a9
Ruslan Kazakov <ruslan.kazakov@gmail.com>
2016-04-06 16:07:42 +1000

    Simple route with Nickel

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,17 @@
-#[macro_use] extern crate nickel;
+#[macro_use]
+extern crate nickel;
 
-use nickel::{Nickel, HttpRouter};
+use nickel::{Nickel, JsonBody, HttpRouter, Request, Response, MiddlewareResult, MediaType};
 
 fn main() {
     let mut server = Nickel::new();
-    server.get("**", middleware!("Hello, Rust!!!"));
-    server.listen("127.0.0.1:6767");
+    let mut router = Nickel::router();
+
+    router.get("/test", middleware! { |request, response|
+        format!("Hello from GET /test")
+    });
+
+    server.utilize(router);
+
+    server.listen("127.0.0.1:9000");
 }
